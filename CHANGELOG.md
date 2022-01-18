0ed3c84b5dd8a4f2caeb17825a38fc772b4d7d94
thim81 <timhaselaars@gmail.com>
2022-01-18 19:16:58 +0100

    Jest testing (#2)
    * Use Jest as testing framework
    
    * Set coding style
    
    Co-authored-by: Tim <>

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+## unreleased
+
+## [1.0.2] - 2021-03-16
+
+- Corrected typo's
+
+## [1.0.1] - 2021-04-03
+
+- Corrected bin command
+
+## [1.0.0] - 2021-04-03
+
+Initial release
+
+- [x] Order AsyncAPI fields in a default order
+- [x] Order AsyncAPI fields in a custom order
+- [x] Filter AsyncAPI files based on operations
+- [x] Filter AsyncAPI files based on flags
+- [x] Filter AsyncAPI files based on tags
+- [x] Filter AsyncAPI files based on operationID's
+- [x] Rename the AsyncAPI title
+- [x] Support AsyncAPI documents in JSON format
+- [x] Support AsyncAPI documents in YAML format
+- [x] Format via CLI
+- [x] Format via config files
+- [x] Use as a Module
+- [x] Support for AsyncAPI 2.0
--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ const jy = require('js-yaml');
 const asyncapiFormat = require('../asyncapi-format.js');
 
 const tests = fs.readdirSync(__dirname).filter(file => {
-    return fs.statSync(path.join(__dirname, file)).isDirectory() && (!file.startsWith('_') || doPrivate);
+  return fs.statSync(path.join(__dirname, file)).isDirectory() && (!file.startsWith('_') || doPrivate);
 });
 
 // SELECTIVE TESTING DEBUG
@@ -16,128 +16,128 @@ const tests = fs.readdirSync(__dirname).filter(file => {
 // console.log('tests',tests);
 
 describe('asyncapi-format tests', () => {
-    tests.forEach((test) => {
-        describe(test, () => {
-            it('should match expected output', (done) => {
-                let options = {};
-                let configFile = null;
-                let configFileOptions = {};
-                let sortOptions = {sortSet: {}};
-                let sortFile = null;
-                let filterFile = null;
-                let filterOptions = {filterSet: {}}
-                let inputFilename = null;
-                let input = null;
+  tests.forEach((test) => {
+    describe(test, () => {
+      it('should match expected output', (done) => {
+        let options = {};
+        let configFile = null;
+        let configFileOptions = {};
+        let sortOptions = {sortSet: {}};
+        let sortFile = null;
+        let filterFile = null;
+        let filterOptions = {filterSet: {}}
+        let inputFilename = null;
+        let input = null;
 
-                try {
-                    // Load options.yaml
-                    configFile = path.join(__dirname, test, 'options.yaml');
-                    configFileOptions = jy.load(fs.readFileSync(configFile, 'utf8'));
-                    configFileOptions.sort = !(configFileOptions['no-sort'])
-                    options = Object.assign({}, options, configFileOptions);
-                } catch (ex) {
-                    // console.error('ERROR Load options.yaml', ex)
-                    try {
-                        // Fallback to options.json
-                        configFile = path.join(__dirname, test, 'options.json');
-                        configFileOptions = JSON.parse(fs.readFileSync(configFile, 'utf8'));
-                        if (configFileOptions['no-sort'] && configFileOptions['no-sort'] === true) {
-                            configFileOptions.sort = !(configFileOptions['no-sort'])
-                            delete configFileOptions['no-sort'];
-                        }
-                        options = Object.assign({}, options, configFileOptions);
-                    } catch (ex) {
-                        // No options found. options = {} will be used
-                        // console.error('ERROR Load options.json', ex)
-                    }
-                }
+        try {
+          // Load options.yaml
+          configFile = path.join(__dirname, test, 'options.yaml');
+          configFileOptions = jy.load(fs.readFileSync(configFile, 'utf8'));
+          configFileOptions.sort = !(configFileOptions['no-sort'])
+          options = Object.assign({}, options, configFileOptions);
+        } catch (ex) {
+          // console.error('ERROR Load options.yaml', ex)
+          try {
+            // Fallback to options.json
+            configFile = path.join(__dirname, test, 'options.json');
+            configFileOptions = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+            if (configFileOptions['no-sort'] && configFileOptions['no-sort'] === true) {
+              configFileOptions.sort = !(configFileOptions['no-sort'])
+              delete configFileOptions['no-sort'];
+            }
+            options = Object.assign({}, options, configFileOptions);
+          } catch (ex) {
+            // No options found. options = {} will be used
+            // console.error('ERROR Load options.json', ex)
+          }
+        }
 
-                try {
-                    // Load customSort.yaml
-                    sortFile = path.join(__dirname, test, 'customSort.yaml');
-                    sortOptions.sortSet = jy.load(fs.readFileSync(sortFile, 'utf8'));
-                    options = Object.assign({}, options, sortOptions);
-                } catch (ex) {
-                    // console.error('ERROR Load customSort.yaml', ex)
-                    try {
-                        // Fallback to customSort.json
-                        sortFile = path.join(__dirname, test, 'customSort.json');
-                        sortOptions.sortSet = JSON.parse(fs.readFileSync(sortFile, 'utf8'));
-                        options = Object.assign({}, options, sortOptions);
-                    } catch (ex) {
-                        // No options found. defaultSort.json will be used
-                        // console.error('ERROR Load customSort.json', ex)
-                        options.sortSet = require('../defaultSort.json')
-                    }
-                }
+        try {
+          // Load customSort.yaml
+          sortFile = path.join(__dirname, test, 'customSort.yaml');
+          sortOptions.sortSet = jy.load(fs.readFileSync(sortFile, 'utf8'));
+          options = Object.assign({}, options, sortOptions);
+        } catch (ex) {
+          // console.error('ERROR Load customSort.yaml', ex)
+          try {
+            // Fallback to customSort.json
+            sortFile = path.join(__dirname, test, 'customSort.json');
+            sortOptions.sortSet = JSON.parse(fs.readFileSync(sortFile, 'utf8'));
+            options = Object.assign({}, options, sortOptions);
+          } catch (ex) {
+            // No options found. defaultSort.json will be used
+            // console.error('ERROR Load customSort.json', ex)
+            options.sortSet = require('../defaultSort.json')
+          }
+        }
 
-                try {
-                    // Load customFilter.yaml
-                    filterFile = path.join(__dirname, test, 'customFilter.yaml');
-                    filterOptions.filterSet = jy.load(fs.readFileSync(filterFile, 'utf8'));
-                    options = Object.assign({}, options, filterOptions);
-                } catch (ex) {
-                    // console.error('ERROR Load customSort.yaml', ex)
-                    try {
-                        // Fallback to customFilter.json
-                        filterFile = path.join(__dirname, test, 'customFilter.json');
-                        filterOptions.filterSet = jy.load(fs.readFileSync(filterFile, 'utf8'));
-                        options = Object.assign({}, options, filterOptions);
-                    } catch (ex) {
-                        // No options found. defaultSort.json will be used
-                        // console.error('ERROR Load customSort.json', ex)
-                        options.filterSet = require('../defaultFilter.json')
-                    }
-                }
+        try {
+          // Load customFilter.yaml
+          filterFile = path.join(__dirname, test, 'customFilter.yaml');
+          filterOptions.filterSet = jy.load(fs.readFileSync(filterFile, 'utf8'));
+          options = Object.assign({}, options, filterOptions);
+        } catch (ex) {
+          // console.error('ERROR Load customSort.yaml', ex)
+          try {
+            // Fallback to customFilter.json
+            filterFile = path.join(__dirname, test, 'customFilter.json');
+            filterOptions.filterSet = jy.load(fs.readFileSync(filterFile, 'utf8'));
+            options = Object.assign({}, options, filterOptions);
+          } catch (ex) {
+            // No options found. defaultSort.json will be used
+            // console.error('ERROR Load customSort.json', ex)
+            options.filterSet = require('../defaultFilter.json')
+          }
+        }
 
-                try {
-                    // Load input.yaml
-                    inputFilename = path.join(__dirname, test, 'input.yaml');
-                    input = jy.load(fs.readFileSync(inputFilename, 'utf8'));
-                } catch (ex) {
-                    // console.error('ERROR Load input.yaml', ex)
+        try {
+          // Load input.yaml
+          inputFilename = path.join(__dirname, test, 'input.yaml');
+          input = jy.load(fs.readFileSync(inputFilename, 'utf8'));
+        } catch (ex) {
+          // console.error('ERROR Load input.yaml', ex)
 
-                    // Fallback to customSort.json
-                    inputFilename = path.join(__dirname, test, 'input.json');
-                    input = jy.load(fs.readFileSync(inputFilename, 'utf8'));
-                }
+          // Fallback to customSort.json
+          inputFilename = path.join(__dirname, test, 'input.json');
+          input = jy.load(fs.readFileSync(inputFilename, 'utf8'));
+        }
 
-                // DEBUG
-                // console.log('options', options)
-                // console.log('inputFilename', inputFilename)
+        // DEBUG
+        // console.log('options', options)
+        // console.log('inputFilename', inputFilename)
 
-                const outputFilename = path.join(__dirname, test, options.output);
-                let readOutput = false;
-                let output = {};
-                try {
-                    output = jy.load(fs.readFileSync(outputFilename, 'utf8'));
-                    readOutput = true;
-                } catch (ex) {
-                    // No options found. output = {} will be used
-                }
+        const outputFilename = path.join(__dirname, test, options.output);
+        let readOutput = false;
+        let output = {};
+        try {
+          output = jy.load(fs.readFileSync(outputFilename, 'utf8'));
+          readOutput = true;
+        } catch (ex) {
+          // No options found. output = {} will be used
+        }
 
-                let result = asyncapiFormat.asyncapiSort(input, options);
-                if (options.filterSet) {
-                    result = asyncapiFormat.asyncapiFilter(result, options);
-                }
+        let result = asyncapiFormat.asyncapiSort(input, options);
+        if (options.filterSet) {
+          result = asyncapiFormat.asyncapiFilter(result, options);
+        }
 
-                // Rename title AsyncAPI document
-                if (options.rename) {
-                    result = asyncapiFormat.asyncapiRename(result, options);
-                }
+        // Rename title AsyncAPI document
+        if (options.rename) {
+          result = asyncapiFormat.asyncapiRename(result, options);
+        }
 
-                if (!readOutput) {
-                    if ((options.output && options.output.indexOf('.json') >= 0) || options.json) {
-                        output = JSON.stringify(result, null, 2);
-                    } else {
-                        output = jy.dump(result);
-                    }
-                    fs.writeFileSync(outputFilename, output, 'utf8');
-                }
+        if (!readOutput) {
+          if ((options.output && options.output.indexOf('.json') >= 0) || options.json) {
+            output = JSON.stringify(result, null, 2);
+          } else {
+            output = jy.dump(result);
+          }
+          fs.writeFileSync(outputFilename, output, 'utf8');
+        }
 
-                assert.deepStrictEqual(result, output);
-                return done();
-            });
-        });
+        assert.deepStrictEqual(result, output);
+        return done();
+      });
     });
+  });
 });
